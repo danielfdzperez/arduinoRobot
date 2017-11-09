0b4ea6a963a2ba28cbbc9cb283ab97de39be6692
danielfdzperez <daniel.fdz.perez@gmail.com>
2017-11-09 19:36:04 +0100

    lcd con libreria

--- lcd.c
+++ lcd.c
@@ -1,5 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "common.h"
 #define LCD_START 20000 //20ms
 #define D0 0
 #define D1 0
@@ -13,7 +14,8 @@
 #define D8 0
 #define D9 0
 #define D11 0
-#define miWait(c) _delay_ms(c/100)
+//#define miWait(c) _delay_ms(c/100)
+#define miWait(c) _delay_ms(2000)
 
 
 /* void miWait(int cicles){ */
@@ -27,45 +29,51 @@
 int configureLCD(){
      miWait(LCD_START);
  
-    PORTB &= ~(1<<4); //RS escribir 0
-    PORTD |= (1<<4); //D4 escribir 1
-    PORTD &= ~(1<<7); //D6 escribir 0
-    PORTC |= (1<<6);//D5 1
-    PORTE &= ~(1<<6);//D7 0
+    //PORTB &= ~(1<<4); //RS escribir 0
+    writeLow(D8);
+    //PORTD |= (1<<4); //D4 escribir 1
+    writeHigh(D4);
+    //PORTD &= ~(1<<7); //D6 escribir 0
+    writeLow(D6);
+    //PORTC |= (1<<6);//D5 1
+    writeHigh(D5);
+    //PORTE &= ~(1<<6);//D7 0
+    writeLow(D6);
 
 
 
 
-    PORTB |= (1<<5);
-    PORTB &= ~(1<<5);
+    writeHigh(D9);
+    write(D9,LOW);
 
     miWait(5000);
 
-    PORTB |= (1<<5);
-    PORTB &= ~(1<<5);
+    writeHigh(D9);
+    write(D9,LOW);
     miWait(200);
-    PORTB |= (1<<5);
-    PORTB &= ~(1<<5);
+    writeHigh(D9);
+    write(D9,LOW);
     miWait(1);
     PORTD &= ~(1<<4); //D4 escribir 0
     miWait(1);
-    PORTB |= (1<<5);
-    PORTB &= ~(1<<5);
+    writeHigh(D9);
+    write(D9,LOW);
     miWait(50);
 
-    PORTB |= (1<<5);
-    PORTB &= ~(1<<5);
+    writeHigh(D9);
+    write(D9,LOW);
 
     PORTE |= (1<<6);//D7 1
     PORTD &= ~(1<<7); //D6 escribir 0
-    PORTB |= (1<<5);
-    PORTB &= ~(1<<5);
+    writeHigh(D9);
+    write(D9,LOW);
     miWait(40);
 }
 
 int main(){
    DDRC |= (1<<7);
-    DDRE |= (1 << 6); //Pin D7 salida
+    //DDRE |= (1 << 6); //Pin D7 salida
+    pinOutput(D7);
     //D7_OUTPUT
     DDRD |= ((1<<4) | (1<<7));// Pin D4 y D6 salida
     DDRC |= (1<<6); //Pin D5 salida
@@ -140,11 +148,11 @@ int main(){
     PORTB &= ~(1<<5);
     while(1) {
       /* set pin 5 high to turn led on */
-      PORTC |= (1<<7);
+      write(D13,HIGH);
       _delay_ms(1000);
 
       /* set pin 5 low to turn led off */
-      PORTC &= ~(1<<7);
+      writeLow(D13);
       _delay_ms(1000);
     }
     return 0;
