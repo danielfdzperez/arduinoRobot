4f9fcdfa33b7bb52edcece286ce7cb7866433441
Miguel García <gtaboadamiguel@gmail.com>
2018-01-10 19:53:24 +0100

    Cambiado el motor para que funcione con el timer de 64 MHz

--- motor.ino
+++ motor.ino
@@ -4,11 +4,11 @@
 #define Ki 0.11
 #define Kp 0.099
 #define Kd 0.0003
-
+#define TOP_MOTOR 200
 // Pines para control del puente h
 #define H1 1  // A4, PINF
 #define H2 0  // A5, PINF
-#define EN 6//D10, PINB
+#define EN 7//D13, PINC
 // Pin del ecoder
 
 #define EC 1 // D2, PIND, INT1
@@ -29,7 +29,7 @@ double ciclo_trabajo_1 = 0;
 PID PID1(&incremento_1,&ciclo_trabajo_1,&objetivo_1, Kp, Ki,Kd,DIRECT);
 PID PID2(&incremento_2,&ciclo_trabajo_2,&objetivo_2, Kp, Ki,Kd,DIRECT);
 int normalizar(double n){
-  return (int)( n * (400.0/255.0)) + 275;//
+  return (int)( n * (200.0/255.0)) + 100;//
 }
 
 void setup(){
@@ -45,22 +45,29 @@ void setup(){
 
   /******************************************************/
   /****** Configuración del pin de enable con PWM *******/
-  /************** Se utiliza timer 1 ********************/
+  /************** Se utiliza timer 4 ********************/
   /******************************************************/
   /******************************************************/
 
 
-  /** Modo correcto en fase y frecuencia -> WGM3:0 = 8  */
+  /** Modo correcto en fase y frecuencia -> WGM4 = 1  */
 
+  TCCR4A = (1 <<COM4A1);//output OC4A (D13)
+  TCCR4B = (1<<CS41); // prescaler 8
+  TCCR4C =(1 << COM4D1); // output OC4D (D6)
+  TCCR4D = (1<<WGM40); //PFC
+  TCCR4E = 0;
 
-  TCCR1A = (1<<COM1B1)|(1<<COM1C1); //COM1x a 0 cuando subiendo y a 1 cuando bajando->2
-  TCCR1B = (1<<WGM13)|(1<<CS10); // Modo correcto en fase y frecuencia con TOP en ICRn, prescaler de 1
-  TCCR1C = 0;
-  ICR1 = 400;
+  OCR4C = TOP_MOTOR;
+
+  /* TCCR1A = ( 1<<COM1B1)|(1<<COM1C1); //COM1x a 0 cuando subiendo y a 1 cuando bajando->2 */
+  /* TCCR1B = (1<<WGM13)|(1<<CS10); // Modo correcto en fase y frecuencia con TOP en ICRn, prescaler de 1 */
+  /* TCCR1C = 0; */
+  /* ICR1 = 400; */
   //TIMSK1 = (1<<OCIE
   /** Ciclo de trabajo del motor a 0% inicialmente*/
 
-  OCR1B = 0;
+  OCR4A = 0;
   //  OCR1C = 0;
 
   /** Interrupcion del encoder */
@@ -88,7 +95,7 @@ void loop(){
   //ciclo_trabajo_1 +=10;
   PID1.Compute();
   PID2.Compute();
-  OCR1B = normalizar(ciclo_trabajo_1);   //normalizar(ciclo_trabajo_1);
+  OCR4A = normalizar(ciclo_trabajo_1);   //normalizar(ciclo_trabajo_1);
   //PORTB |= (1<<EN);
   //digitalWrite(9,HIGH);
   _delay_ms(300);
